1fd82d283b1053431207a37283dcffce4bf4aa47
Marcello Mulas <marcellomulas93@gmail.com>
2023-05-23 15:12:06 +0200

    Logic score added/Feat final screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //import { useState } from "react";
 import InitialPage from "./components/InitialPage";
 import QuestionBox from "./components/Question";
+import EndScreen from "./components/EndScreen";
 import "./App.css";
 import { useState } from "react";
 import { QuestionData } from "./QuestionData";
@@ -8,22 +9,33 @@ import { QuestionData } from "./QuestionData";
 function App() {
   const [quizStarted, setQuizStarted] = useState(false);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
   const startQuiz = () => {
     setQuizStarted(true);
   };
 
   const onAnswer = (answerIndex: number) => {
+    if (answerIndex === QuestionData[currentQuestionIndex].correctAnswerIndex) {
+      setScore(score + QuestionData[currentQuestionIndex].score);
+    }
+
     setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
+
   if (quizStarted) {
-    return (
-      <div>
-        <QuestionBox
-          question={QuestionData[currentQuestionIndex]}
-          onAnswer={onAnswer}
-        />
-      </div>
-    );
+    if (currentQuestionIndex < QuestionData.length) {
+      return (
+        <div>
+          <QuestionBox
+            question={QuestionData[currentQuestionIndex]}
+            onAnswer={onAnswer}
+          />
+        </div>
+      );
+    } else {
+      return <EndScreen finalScore={score} />;
+    }
   } else {
     return (
       <>
