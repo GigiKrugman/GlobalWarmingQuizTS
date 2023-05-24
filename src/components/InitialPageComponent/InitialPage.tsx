f606d52223dff5e15613114e68fd4ded1f750521
Marcello Mulas <marcellomulas93@gmail.com>
2023-05-24 15:19:22 +0200

    Style added

--- src/components/InitialPageComponent/InitialPage.tsx
+++ src/components/InitialPageComponent/InitialPage.tsx
@@ -11,20 +11,26 @@ export default function InitialPage({ startQuiz }: InitialPageProps) {
       <h2 className="initial--screen--sub-title">
         The ultimate quiz to test your knowledge regarding the Global Warming!{" "}
       </h2>
-      <h3>Rules of the game:</h3>
-      <h4>
+      <h3 className="initial--screen--h3">Rules of the game:</h3>
+      <h4 className="initial--screen--h4">
         There are 10 Questions regarding the climate change and global warming:
       </h4>
-      <p>
+      <p className="initial--screen--paragraph">
         Each Questions has a different grade of difficulty, so the relative
         score will be different!
       </p>
-      <ol>
-        <li>Easy - 1 point</li>
-        <li>Medium - 2 point</li>
-        <li>Difficult - 3 point</li>
-      </ol>
-      <h3>Ready to Take the Challenge?</h3>
+      <ul className="initial--screen--ul">
+        <li>
+          <span className="span--easy">Easy</span> - 1 point
+        </li>
+        <li>
+          <span className="span--medium">Medium</span> - 2 point
+        </li>
+        <li>
+          <span className="span--hard">Difficult</span> - 3 point
+        </li>
+      </ul>
+      <h4 className="initial--screen--h4">Ready to Take the Challenge?</h4>
       <button onClick={startQuiz} className="initial--screen--btn">
         Let's Start
       </button>
